443585323fe8832c891676defb3d52e4319c6d69
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-11 18:32:43 +0900

    Feat : GameManager, PoolManager 추가

---
+++ Assets/01.Scripts/Managers/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    /* PoolManager 사용 예시
+    public GameObject bear_Prefab;
+    public GameObject SpawnBear() { return PoolManager.Instance.Pop(bear_Prefab); }
+    public void DespawnBear(GameObject bear) { PoolManager.Instance.Push(bear); }
+    */
+
+}
