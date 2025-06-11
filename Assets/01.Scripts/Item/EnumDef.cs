0b07571ac03ff8013f0e3f58b2a198f7c7a514a4
DounPark <ehdns1562@gmail.com>
2025-06-11 21:05:24 +0900

    add scripts about item

---
+++ Assets/01.Scripts/Item/EnumDef.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemRarity { Normal, Magic, Rare, Unique, Set }
+public enum ItemType
+{
+    Sword, Axe, Bow, Crossbow, Staff, Wand,
+    Helmet, Armor, Belt, Boots, Gloves, Cloak,
+    Necklace, Ring1, Ring2
+}
+public enum CharacterClass { Warrior, Archer, Mage }
