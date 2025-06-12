ea2eda4d7396d2d77d8535c63fd273ea51a86810
DounPark <ehdns1562@gmail.com>
2025-06-12 13:08:12 +0900

    add itemgenerator

--- Assets/05.Data/ItemData/UniqueItemData.cs
+++ Assets/05.Data/ItemData/UniqueItemData.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UniqueItemData : MonoBehaviour
+[CreateAssetMenu(menuName = "Item/Unique Item")]
+public class UniqueItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string uniqueName;
+    public ItemType itemType;
+    public CharacterClass usableClass;
+    public List<ItemOption> fixedOptions; // 옵션 4줄
+    public ItemOption uniqueOption; // 고유 옵션
 }
