7553f2aa72919c1a138792b4543a241c6baa33c1
DounPark <ehdns1562@gmail.com>
2025-06-13 01:14:32 +0900

    add CSV, OptionPoolGenerator

--- Assets/01.Scripts/Item/EnumDef.cs
+++ Assets/01.Scripts/Item/EnumDef.cs
@@ -10,3 +10,10 @@ public enum ItemType
     Necklace, Ring1, Ring2
 }
 public enum CharacterClass { Warrior, Archer, Mage }
+
+public enum ItemOptionType
+{
+    공격력, 방어력, 이동속도, 공격속도, 치명타확률, 치명타피해량, 
+    행운, 최대HP량증가, 최대MP량증가, 초당HP회복량증가, 초당MP회복량증가, 초당HP회복속도증가, 초당MP회복속도증가,
+    힘, 민첩, 지능, 투사체수증가, 투사체속도증가, 특수공격력증가, 공격범위증가
+}
--- Assets/01.Scripts/Item/ItemOption.cs
+++ Assets/01.Scripts/Item/ItemOption.cs
@@ -14,12 +14,53 @@ public class RarityRange
 [System.Serializable]
 public class ItemOptionWithRarity
 {
-    public string optionName;
+    public ItemOptionType optionType;
     public List<RarityRange> rarityRanges;
 
     public float GetRandomValue(ItemRarity rarity)
     {
         var range = rarityRanges.FirstOrDefault(r => r.rarity == rarity);
-        return range != null ? UnityEngine.Random.Range(range.minValue, range.maxValue) : 0f;
+        return range != null ? Random.Range(range.minValue, range.maxValue) : 0f;
     }
+}
+
+[System.Serializable]
+public class FixedItemOption
+{
+    public ItemOptionType optionType;
+    public float minValue;
+    public float maxValue;
+    
+
+    public float GetValue()
+    {
+        return Random.Range(minValue, maxValue);
+    }
+}
+
+[System.Serializable]
+public class UniqueItemOption
+{
+    public string optionName;
+    public float minValue;
+    public float maxValue;
+    
+
+    public float GetValue()
+    {
+        return Random.Range(minValue, maxValue);
+    }
+}
+
+[System.Serializable]
+public class SetBonusOption
+{
+    [Tooltip("몇 개 착용 시 발동되는 보너스")]
+    public int requiredCount;
+
+    public string optionName;
+    public float minValue;
+    public float maxValue;
+
+    public float GetValue() => UnityEngine.Random.Range(minValue, maxValue);
 }
