d7dc8fc8acb8179ff3a0b5628fb5d9fec9d99c00
GUYoof <gom.nyusuk@gmail.com>
2025-06-13 15:36:42 +0900

    typo Refactor : 카메라 경계 삭제
    typo Refactor : 카메라 경계 삭제

--- Assets/01.Scripts/Player/CameraController.cs
+++ Assets/01.Scripts/Player/CameraController.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    Vector2 minCameraBoundary;
-    [SerializeField]
-    Vector2 maxCameraBoundary;
-    [SerializeField]
     float smoothing = 0.2f;
 
     public GameObject Target;
@@ -33,39 +29,10 @@ public class CameraController : MonoBehaviour
         // 플레이어 위치 기준의 목표 카메라 위치 (Z축은 고정)
         Vector3 targetPos = new Vector3(Target.transform.position.x, Target.transform.position.y, transform.position.z);
 
-        // 카메라 뷰포트의 절반 크기를 고려한 경계 클램핑
-        float cameraHalfHeight = Camera.main.orthographicSize;
-        float cameraHalfWidth = cameraHalfHeight * Camera.main.aspect;
-
-        float minX = minCameraBoundary.x + cameraHalfWidth;
-        float maxX = maxCameraBoundary.x - cameraHalfWidth;
-        float minY = minCameraBoundary.y + cameraHalfHeight;
-        float maxY = maxCameraBoundary.y - cameraHalfHeight;
-
-        // 카메라가 경계 밖으로 넘어가지 않도록 위치 제한
-        targetPos.x = Mathf.Clamp(targetPos.x, minX, maxX);
-        targetPos.y = Mathf.Clamp(targetPos.y, minY, maxY);
-
         // 부드럽게 이동
         transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
 
         //transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, -10);
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-
-        Vector3 center = new Vector3(
-            (minCameraBoundary.x + maxCameraBoundary.x) / 2f,
-            (minCameraBoundary.y + maxCameraBoundary.y) / 2f,
-            transform.position.z);
-
-        Vector3 size = new Vector3(
-            Mathf.Abs(maxCameraBoundary.x - minCameraBoundary.x),
-            Mathf.Abs(maxCameraBoundary.y - minCameraBoundary.y),
-            0f);
-
-        Gizmos.DrawWireCube(center, size);
-    }
 }
