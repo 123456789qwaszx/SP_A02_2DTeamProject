78c3d44daf9b1c3dae02c3fefcc20a64f4a1f9d8
BbangGae <ydh1390@gmail.com>
2025-06-13 19:38:23 +0900

    Lv2맵 추가

---
+++ Assets/01.Scripts/Monster/MonsterStateMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MonsterStateMove : MonsterStateBase
+{
+    public MonsterStateMove(MonsterBase monster, Animator animator) : base(monster, animator) { }
+
+    public override void Enter()
+    {
+        animator.SetBool("Move", true);
+    }
+
+    public override void Update()
+    {
+        monster.MoveToPlayer();
+        monster.Flip();
+        if (monster.CanAttack())
+        {
+            monster.ChangeState(monster.StateMeleeAttack);
+        }
+    }
+
+    public override void Exit()
+    {
+        animator.SetBool("Move", false);
+    }
+}
