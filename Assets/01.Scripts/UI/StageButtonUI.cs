843b0fa7e848c192ac45cc6b3c59b9eb9d548dcc
rlqma1131 <rlqma1131@gmail.com>
2025-06-14 23:56:12 +0900

    feat: StageManager.cs / UIManger.cs
    - StageManager로 각 스테이지 관리 가능
    - UIManager로 스테이지선택 가능

---
+++ Assets/01.Scripts/UI/StageButtonUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageButtonUI : MonoBehaviour
+{
+    [Tooltip("스테이지 번호")]
+    public int stageNumber;
+
+    [Tooltip("스테이지가 속한 씬 이름")]
+    public string targetSceneName;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        if (button != null)
+            button.onClick.AddListener(OnClickStageButton);
+    }
+
+    void OnClickStageButton()
+    {
+        GameManager.Instance.currentStage = stageNumber;
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
