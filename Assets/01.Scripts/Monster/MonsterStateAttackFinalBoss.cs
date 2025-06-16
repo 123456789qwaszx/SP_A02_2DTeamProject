d2705d8e1bee048315bbee1725f6757dbba84c88
rlqma1131 <rlqma1131@gmail.com>
2025-06-16 16:13:27 +0900

    feat: 최종보스 진행중

--- Assets/01.Scripts/Monster/MonsterStateAttackFinalBoss.cs
+++ Assets/01.Scripts/Monster/MonsterStateAttackFinalBoss.cs
@@ -11,14 +11,7 @@ public class MonsterStateAttackFinalBoss : MonsterStateBase
 
     public override void Enter()
     {
-        float hpPercent = finalBoss.HPPercent;
-
-        if (hpPercent > 0.6f)
-            animator.SetTrigger("Attack_Phase1"); // 단일 투사체
-        else if (hpPercent > 0.3f)
-            animator.SetTrigger("Attack_Phase2"); // 3갈래 + 장판
-        else
-            animator.SetTrigger("Attack_Phase3"); // 장판폭발 or 돌진
+        animator.SetTrigger("Attack");
     }
 
     public override void Update()
