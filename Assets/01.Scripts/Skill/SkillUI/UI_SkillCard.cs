d0fae749d017945bd7e9668976feaf39582b3d08
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-17 14:57:49 +0900

    Fix : 스킬레벨에 따른 스킬카드 표시 추가, 스킬선택UI폴리싱

--- Assets/01.Scripts/Skill/SkillUI/UI_SkillCard.cs
+++ Assets/01.Scripts/Skill/SkillUI/UI_SkillCard.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 
 public class UI_SkillCard : MonoBehaviour
 {
-
-    // 스킬 Name
-    // 스킬 Description
-
     private SkillBase _skill;
 
     public TextMeshProUGUI _skillName;
@@ -18,8 +14,12 @@ public class UI_SkillCard : MonoBehaviour
     public TextMeshProUGUI _skillNextLevel;
     public Image _skillIcon;
 
-    //string description
-    //string SkillName;
+    public Image _starOn_1;
+    public Image _starOn_2;
+    public Image _starOn_3;
+    public Image _starOn_4;
+    public Image _starOn_5;
+
 
     public void SetInfo(SkillBase skill)
     {
@@ -33,19 +33,21 @@ public class UI_SkillCard : MonoBehaviour
         _skillDescription.text = _skill.SkillData.Description;
         _skillCurLevel.text = $"{SkillManager.Instance.SavedBattleSkill[_skill.SkillType]}";
         _skillNextLevel.text = $"{SkillManager.Instance.SavedBattleSkill[_skill.SkillType] + 1}";
-        //_skillIcon = GetComponent<Image>();
         _skillIcon.sprite = _skill.SkillData.Icon;
 
+        // 별세팅 만약 6개가 꽉찬 상태라면 셀렉트창에 나오지 않기때문에 0은 제외함.
+        _starOn_1.gameObject.SetActive(_skill.Level + 1 >= 2);
+        _starOn_2.gameObject.SetActive(_skill.Level + 1 >= 3);
+        _starOn_3.gameObject.SetActive(_skill.Level + 1 >= 4);
+        _starOn_4.gameObject.SetActive(_skill.Level + 1 >= 5);
+        _starOn_5.gameObject.SetActive(_skill.Level + 1 >= 6);
 
-        // 별세팅
-        // GetImage((int)Images.StarOn_1).gameObject.SetActive(_skill.Level + 1 >= 2);
     }
 
     public void OnClicked()
     {
         GameManager.Instance.controller.sklilbook.LevelUpSkill(_skill.SkillType);
-        Debug.Log(_skill.SkillType);
-        Debug.Log(SkillManager.Instance.SavedBattleSkill[_skill.SkillType]);
         // UI 닫기
+        //ResourceManager.Instance.Destroy(gameObject);
     }
 }
