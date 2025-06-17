d99313d123df758fc3b6a2cf034741f3cba92012
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-17 22:06:00 +0900

    Fix : 플레이어 컴포넌트 수정
    플레이어에 동일 컴포넌트들 제거

--- Assets/01.Scripts/Skill/SkillBook.cs
+++ Assets/01.Scripts/Skill/SkillBook.cs
@@ -9,46 +9,6 @@ public class SkillBook : MonoBehaviour
     public static int MAX_SKILL_LEVEL = 6;
     public static int MAX_SKILL_COUNT = 6;
 
-    void Start()
-    {
-        ResourceManager.Instance.LoadAllAsync<GameObject>("Skill_Prefabs", (key, count, totalCount) =>
-        {
-            Debug.Log($"{key} {count}/{totalCount}");
-
-            if (count == totalCount)
-            {
-                SkillManager.Instance.StartSkillLoad();
-
-                SkillType HolyProjectile = SkillManager.Instance.GetSkillTypeFromInt(10001);
-                SkillType HolyPulse = SkillManager.Instance.GetSkillTypeFromInt(10011);
-                SkillType DarkArrow = SkillManager.Instance.GetSkillTypeFromInt(10021);
-                SkillType WindCutter = SkillManager.Instance.GetSkillTypeFromInt(10071);
-                SkillType BloodChain = SkillManager.Instance.GetSkillTypeFromInt(10111);
-
-
-                // 처음 무조건 들고 있는 스킬들.
-                // 직업 추가 등의 이유로 바꾸거나, 종류를 늘려주고 싶다면
-                // 뒤의 SkillIndex를 바꾸거나 추가로 AddSkill()을 할 것.
-                AddSkill(HolyProjectile, 10001);
-                //LevelUpSkill(HolyProjectile);
-
-                AddSkill(HolyPulse, 10011);
-                //LevelUpSkill(HolyPulse);
-
-                AddSkill(DarkArrow, 10021);
-                //LevelUpSkill(DarkArrow);
-
-                AddSkill(WindCutter, 10071);
-                LevelUpSkill(WindCutter);
-
-                AddSkill(BloodChain, 10111);
-                //LevelUpSkill(BloodChain);
-                // 이렇게 직접 레벨업 시키는 건 처음 시작시 세팅만 이렇고, 이후는 스킬카드UI의 버튼을 통해 LevelUpSkill이 실행됨
-
-            }
-        });
-    }
-
     
     public void LoadSkill(SkillType skillType, int level)
     {
