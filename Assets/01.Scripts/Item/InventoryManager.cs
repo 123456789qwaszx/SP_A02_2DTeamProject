a6d5854e151fb5807fd7f2d01dd7ba2932c44c36
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-09-02 06:46:08 +0900

    refactor : 인벤토리 매니저 리팩토링 계획
    1. 인벤토리 매니저는 DontDestroyOnLoad지만, 연결되어야 하는 패널들이, 일반 게임오브젝트여서 연결이 풀려버림.
    2. 따라서 각 패널들을 UI_Base로 편입 시킬 것.
    3. 그리고 InventoryUIController는 Scene전체에서 쓰이는 공용버튼을 담당하고 있는데, 다른 곳으로 이동시키기. 그리고 참조하고 있는 3종류의 UI는 UIManager를 통해 관리하기.

--- Assets/01.Scripts/Item/InventoryManager.cs
+++ Assets/01.Scripts/Item/InventoryManager.cs
@@ -3,27 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+public class InventoryManager : Singleton<InventoryManager>
 {
-    public static InventoryManager Instance { get; private set; }
-
     private List<GeneratedItem> items = new List<GeneratedItem>();
     
     [SerializeField] private int maxSlotCount = 30;
     public int MaxSlotCount => maxSlotCount;
 
     public List<GeneratedItem> Items => new List<GeneratedItem>(items); // 항상 복사본 제공
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject); // 필요시
-    }
 
     /// <summary>
     /// 인벤토리에 아이템 추가
